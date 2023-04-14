24aea573c851946b98daf14bdca755e4819c2724
P4anix <pancok_1@yahoo.it>
2023-04-14 18:02:03 +0200

    error 500

--- movie.php
+++ movie.php
@@ -6,19 +6,21 @@ class Movie {
     public $titolo;
     public $anno;
     public $regista;
+    public $genere;
     
 
     // definisco un costruttore
-    public function __construct($titolo, $anno, $regista) {
+    public function __construct($titolo, $anno, $regista, $genere) {
         $this->titolo = $titolo;
         $this->anno = $anno;
         $this->regista = $regista;
+        $this->genere = $genere;
         
     }
 
     // definisco un metodo
     public function informazioni() {
-        return "Sotto la regia di {$this->regista} nel {$this->anno} esce il capolavoro '{$this->titolo}'.";
+        return "Sotto la regia di {$this->regista} nel {$this->anno} esce il capolavoro '{$this->titolo}' che fa parte del genere {$this->genere}.";
     }
 }
 
