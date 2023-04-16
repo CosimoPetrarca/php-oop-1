a23d0fb2199a311aac7dadb71008d388b8f0d523
P4anix <pancok_1@yahoo.it>
2023-04-16 08:27:29 +0200

    fix errore 500

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 // includo il file che contiene la definizione della classe Movie
 require_once('Movie.php');
 
-$movie1 = new Movie("LaCapaGira", 1999, "Alessandro Piva, drammatico");
-$movie2 = new Movie("Così fan tutte", 1992, "Tinto Brass, erotico");
-$movie3 = new Movie("Per un pugno di dollari", 1964, "Sergio Leone, azione");
+$movie1 = new Movie("LaCapaGira", 1999, "Alessandro Piva", "comico");
+$movie2 = new Movie("Così fan tutte", 1992, "Tinto Brass", "erotico");
+$movie3 = new Movie("Per un pugno di dollari", 1964, "Sergio Leone", "azione");
 
 // stampo a schermo dei valori delle relative proprietà
 echo $movie1->informazioni() . "<br>";
